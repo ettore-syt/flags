1e0cdedb3cbfddea5157dd41eaf7bd01f2364b9b
Ettore Mihaili <ettore@superyachttimes.com>
2024-06-21 09:56:48 +0200

    initial commit

---
+++ index.tsx
@@ -0,0 +1,433 @@
+import AalandIslandsFlag from "./svg/AalandIslands";
+import AfghanistanFlag from "./svg/Afghanistan";
+import AlbaniaFlag from "./svg/Albania";
+import AlgeriaFlag from "./svg/Algeria";
+import AndorraFlag from "./svg/Andorra";
+import AngolaFlag from "./svg/Angola";
+import AntiguaBarbudaFlag from "./svg/AntiguaBarbuda";
+import ArgentinaFlag from "./svg/Argentina";
+import ArmeniaFlag from "./svg/Armenia";
+import AustraliaFlag from "./svg/Australia";
+import AustriaFlag from "./svg/Austria";
+import AzerbaijanFlag from "./svg/Azerbaijan";
+import BahamasFlag from "./svg/Bahamas";
+import BahrainFlag from "./svg/Bahrain";
+import BangladeshFlag from "./svg/Bangladesh";
+import BarbadosFlag from "./svg/Barbados";
+import BelarusFlag from "./svg/Belarus";
+import BelgiumFlag from "./svg/Belgium";
+import BelizeFlag from "./svg/Belize";
+import BeninFlag from "./svg/Benin";
+import BhutanFlag from "./svg/Bhutan";
+import BoliviaFlag from "./svg/Bolivia";
+import BosniaHerzegovinaFlag from "./svg/BosniaHerzegovina";
+import BotswanaFlag from "./svg/Botswana";
+import BrazilFlag from "./svg/Brazil";
+import BritishVirginIslandsFlag from "./svg/BritishVirginIslands";
+import BruneiFlag from "./svg/Brunei";
+import BulgariaFlag from "./svg/Bulgaria";
+import BurkinaFasoFlag from "./svg/BurkinaFaso";
+import BurundiFlag from "./svg/Burundi";
+import CambodiaFlag from "./svg/Cambodia";
+import CameroonFlag from "./svg/Cameroon";
+import CanadaFlag from "./svg/Canada";
+import CapeVerdeFlag from "./svg/CapeVerde";
+import CaymanIslandsFlag from "./svg/CaymanIslands";
+import CentralAfricanRepublicFlag from "./svg/CentralAfricanRepublic";
+import ChadFlag from "./svg/Chad";
+import ChileFlag from "./svg/Chile";
+import ChinaFlag from "./svg/China";
+import ColombiaFlag from "./svg/Colombia";
+import ComorosFlag from "./svg/Comoros";
+import CostaRicaFlag from "./svg/CostaRica";
+import CroatiaFlag from "./svg/Croatia";
+import CubaFlag from "./svg/Cuba";
+import CyprusFlag from "./svg/Cyprus";
+import CzechiaFlag from "./svg/Czechia";
+import DemocraticRepublicOfCongoFlag from "./svg/DemocraticRepublicOfCongo";
+import DenmarkFlag from "./svg/Denmark";
+import DjiboutiFlag from "./svg/Djibouti";
+import DominicaFlag from "./svg/Dominica";
+import DominicanRepublicFlag from "./svg/DominicanRepublic";
+import EastTimorFlag from "./svg/EastTimor";
+import EcuadorFlag from "./svg/Ecuador";
+import EgyptFlag from "./svg/Egypt";
+import ElSalvadorFlag from "./svg/ElSalvador";
+import EnglandFlag from "./svg/England";
+import EquatorialGuineaFlag from "./svg/EquatorialGuinea";
+import EritreaFlag from "./svg/Eritrea";
+import EstoniaFlag from "./svg/Estonia";
+import EthiopiaFlag from "./svg/Ethiopia";
+import EuropeFlag from "./svg/Europe";
+import FalklandIslandsFlag from "./svg/FalklandIslands";
+import FaroeIslandsFlag from "./svg/FaroeIslands";
+import FijiFlag from "./svg/Fiji";
+import FinlandFlag from "./svg/Finland";
+import FranceFlag from "./svg/France";
+import FrenchGuianaFlag from "./svg/FrenchGuiana";
+import GabonFlag from "./svg/Gabon";
+import GambiaFlag from "./svg/Gambia";
+import GeorgiaFlag from "./svg/Georgia";
+import GermanyFlag from "./svg/Germany";
+import GhanaFlag from "./svg/Ghana";
+import GibraltarFlag from "./svg/Gibraltar";
+import GreeceFlag from "./svg/Greece";
+import GrenadaFlag from "./svg/Grenada";
+import GuadeloupeFlag from "./svg/Guadeloupe";
+import GuatemalaFlag from "./svg/Guatemala";
+import GuemseyFlag from "./svg/Guemsey";
+import GuineaFlag from "./svg/Guinea";
+import GuyanaFlag from "./svg/Guyana";
+import HaitiFlag from "./svg/Haiti";
+import HondurasFlag from "./svg/Honduras";
+import HongKongFlag from "./svg/HongKong";
+import HungaryFlag from "./svg/Hungary";
+import IcelandFlag from "./svg/Iceland";
+import IndiaFlag from "./svg/India";
+import IndonesiaFlag from "./svg/Indonesia";
+import IranFlag from "./svg/Iran";
+import IraqFlag from "./svg/Iraq";
+import IrelandFlag from "./svg/Ireland";
+import IsleOfManFlag from "./svg/IsleOfMan";
+import IsraelFlag from "./svg/Israel";
+import ItalyFlag from "./svg/Italy";
+import IvoryCoastFlag from "./svg/IvoryCoast";
+import JamaicaFlag from "./svg/Jamaica";
+import JapanFlag from "./svg/Japan";
+import JerseyFlag from "./svg/Jersey";
+import JordanFlag from "./svg/Jordan";
+import KazakhstanFlag from "./svg/Kazakhstan";
+import KenyaFlag from "./svg/Kenya";
+import KiribatiFlag from "./svg/Kiribati";
+import KosovoFlag from "./svg/Kosovo";
+import KuwaitFlag from "./svg/Kuwait";
+import KyrgyzstanFlag from "./svg/Kyrgyzstan";
+import LaosFlag from "./svg/Laos";
+import LatviaFlag from "./svg/Latvia";
+import LebanonFlag from "./svg/Lebanon";
+import LesothoFlag from "./svg/Lesotho";
+import LiberiaFlag from "./svg/Liberia";
+import LibyaFlag from "./svg/Libya";
+import LiechtensteinFlag from "./svg/Liechtenstein";
+import LithuaniaFlag from "./svg/Lithuania";
+import LuxembourgFlag from "./svg/Luxembourg";
+import MadagascarFlag from "./svg/Madagascar";
+import MalawiFlag from "./svg/Malawi";
+import MalaysiaFlag from "./svg/Malaysia";
+import MaldivesFlag from "./svg/Maldives";
+import MaliFlag from "./svg/Mali";
+import MaltaFlag from "./svg/Malta";
+import MarshallIslandsFlag from "./svg/MarshallIslands";
+import MauritianaFlag from "./svg/Mauritiana";
+import MauritiusFlag from "./svg/Mauritius";
+import MexicoFlag from "./svg/Mexico";
+import MicronesiaFlag from "./svg/Micronesia";
+import MoldovaFlag from "./svg/Moldova";
+import MonacoFlag from "./svg/Monaco";
+import MongoliaFlag from "./svg/Mongolia";
+import MontenegroFlag from "./svg/Montenegro";
+import MoroccoFlag from "./svg/Morocco";
+import MozanbiqueFlag from "./svg/Mozanbique";
+import MyanmarFlag from "./svg/Myanmar";
+import NamibiaFlag from "./svg/Namibia";
+import NauruFlag from "./svg/Nauru";
+import NepalFlag from "./svg/Nepal";
+import NetherlandsFlag from "./svg/Netherlands";
+import NewZealandFlag from "./svg/NewZealand";
+import NicaraguaFlag from "./svg/Nicaragua";
+import NigerFlag from "./svg/Niger";
+import NigeriaFlag from "./svg/Nigeria";
+import NorthKoreaFlag from "./svg/NorthKorea";
+import NorthMacedoniaFlag from "./svg/NorthMacedonia";
+import NorwayFlag from "./svg/Norway";
+import OmanFlag from "./svg/Oman";
+import PakistanFlag from "./svg/Pakistan";
+import PalauFlag from "./svg/Palau";
+import PalestineFlag from "./svg/Palestine";
+import PanamaFlag from "./svg/Panama";
+import PapuaNewGuineaFlag from "./svg/PapuaNewGuinea";
+import ParaguayFlag from "./svg/Paraguay";
+import PeruFlag from "./svg/Peru";
+import PhilippinesFlag from "./svg/Philippines";
+import PolandFlag from "./svg/Poland";
+import PortugalFlag from "./svg/Portugal";
+import PuertoRicoFlag from "./svg/PuertoRico";
+import QatarFlag from "./svg/Qatar";
+import RepublicCongoFlag from "./svg/RepublicCongo";
+import RomaniaFlag from "./svg/Romania";
+import RussiaFlag from "./svg/Russia";
+import RwandaFlag from "./svg/Rwanda";
+import SaintKittsNevisFlag from "./svg/SaintKittsNevis";
+import SaintLuciaFlag from "./svg/SaintLucia";
+import SamoaFlag from "./svg/Samoa";
+import SaintVincentGrenadinesFlag from "./svg/SaintVincentGrenadines"
+import SanMarinoFlag from "./svg/SanMarino";
+import SaoTomePrincipeFlag from "./svg/SaoTomePrincipe";
+import SaudiArabiaFlag from "./svg/SaudiArabia";
+import SenegalFlag from "./svg/Senegal";
+import SerbiaFlag from "./svg/Serbia";
+import SeychellesFlag from "./svg/Seychelles";
+import SierraLeoneFlag from "./svg/SierraLeone";
+import SingaporeFlag from "./svg/Singapore";
+import SlovakiaFlag from "./svg/Slovakia";
+import SloveniaFlag from "./svg/Slovenia";
+import SolomonIslandsFlag from "./svg/SolomonIslands";
+import SomaliaFlag from "./svg/Somalia";
+import SouthAfricaFlag from "./svg/SouthAfrica";
+import SouthGeorgiaSandwichIslandsFlag from "./svg/SouthGeorgiaSandwichIslands";
+import SouthKoreaFlag from "./svg/SouthKorea";
+import SouthSudanFlag from "./svg/SouthSudan";
+import SpainFlag from "./svg/Spain";
+import SriLankaFlag from "./svg/SriLanka";
+import SudanFlag from "./svg/Sudan";
+import SurinameFlag from "./svg/Suriname";
+import SwedenFlag from "./svg/Sweden";
+import SwitzerlandFlag from "./svg/Switzerland";
+import SyriaFlag from "./svg/Syria";
+import TaiwanFlag from "./svg/Taiwan";
+import TajikistanFlag from "./svg/Tajikistan";
+import TanzaniaFlag from "./svg/Tanzania";
+import ThailandFlag from "./svg/Thailand";
+import TogoFlag from "./svg/Togo";
+import TongaFlag from "./svg/Tonga";
+import TrinidadTobagoFlag from "./svg/TrinidadTobago";
+import TunisiaFlag from "./svg/Tunisia";
+import TurkeyFlag from "./svg/Turkey";
+import TurkmenistanFlag from "./svg/Turkmenistan";
+import UgandaFlag from "./svg/Uganda";
+import UkraineFlag from "./svg/Ukraine";
+import UnitedArabEmiratesFlag from "./svg/UnitedArabEmirates";
+import UnitedKingdomFlag from "./svg/UnitedKingdom";
+import UnitedStatesFlag from "./svg/UnitedStates";
+import UruguayFlag from "./svg/Uruguay";
+import UzbekistanFlag from "./svg/Uzbekistan";
+import VenezuelaFlag from "./svg/Venezuela";
+import VietnamFlag from "./svg/Vietnam";
+import YemenFlag from "./svg/Yemen";
+import ZambiaFlag from "./svg/Zambia";
+
+
+export function Flag({ country }: { country: keyof typeof flagComponents }) {
+
+    const FlagComponent = flagComponents[country];
+
+    if (!FlagComponent) {
+        return <div>{country}</div>;
+    }
+
+    return (
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" viewBox="0 0 32 32">
+                    <FlagComponent />
+                </svg>
+    );
+}
+
+const flagComponents = {
+    "AalandIslands": AalandIslandsFlag,
+    "Afghanistan": AfghanistanFlag,
+    "Albania": AlbaniaFlag,
+    "Algeria": AlgeriaFlag,
+    "Andorra": AndorraFlag,
+    "Angola": AngolaFlag,
+    "Antigua and Barbuda": AntiguaBarbudaFlag,
+    "Argentina": ArgentinaFlag,
+    "Armenia": ArmeniaFlag,
+    "Australia": AustraliaFlag,
+    "Austria": AustriaFlag,
+    "Azerbaijan": AzerbaijanFlag,
+    "Bahamas": BahamasFlag,
+    "Bahrain": BahrainFlag,
+    "Bangladesh": BangladeshFlag,
+    "Barbados": BarbadosFlag,
+    "Belarus": BelarusFlag,
+    "Belgium": BelgiumFlag,
+    "Belize": BelizeFlag,
+    "Benin": BeninFlag,
+    "Bhutan": BhutanFlag,
+    "Bolivia": BoliviaFlag,
+    "BosniaHerzegovina": BosniaHerzegovinaFlag,
+    "Botswana": BotswanaFlag,
+    "Brazil": BrazilFlag,
+    "BritishVirginIslands": BritishVirginIslandsFlag,
+    "Brunei": BruneiFlag,
+    "Bulgaria": BulgariaFlag,
+    "BurkinaFaso": BurkinaFasoFlag,
+    "Burundi": BurundiFlag,
+    "Cambodia": CambodiaFlag,
+    "Cameroon": CameroonFlag,
+    "Canada": CanadaFlag,
+    "CapeVerde": CapeVerdeFlag,
+    "CaymanIslands": CaymanIslandsFlag,
+    "CentralAfricanRepublic": CentralAfricanRepublicFlag,
+    "Chad": ChadFlag,
+    "Chile": ChileFlag,
+    "China": ChinaFlag,
+    "Colombia": ColombiaFlag,
+    "Comoros": ComorosFlag,
+    "CostaRica": CostaRicaFlag,
+    "Croatia": CroatiaFlag,
+    "Cuba": CubaFlag,
+    "Cyprus": CyprusFlag,
+    "Czechia": CzechiaFlag,
+    "DemocraticRepublicOfCongo": DemocraticRepublicOfCongoFlag,
+    "Denmark": DenmarkFlag,
+    "Djibouti": DjiboutiFlag,
+    "Dominica": DominicaFlag,
+    "DominicanRepublic": DominicanRepublicFlag,
+    "EastTimor": EastTimorFlag,
+    "Ecuador": EcuadorFlag,
+    "Egypt": EgyptFlag,
+    "ElSalvador": ElSalvadorFlag,
+    "England": EnglandFlag,
+    "Equatorial Guinea": EquatorialGuineaFlag,
+    "Eritrea": EritreaFlag,
+    "Estonia": EstoniaFlag,
+    "Ethiopia": EthiopiaFlag,
+    "Europe": EuropeFlag,
+    "FalklandIslands": FalklandIslandsFlag,
+    "FaroeIslands": FaroeIslandsFlag,
+    "Fiji": FijiFlag,
+    "Finland": FinlandFlag,
+    "France": FranceFlag,
+    "FrenchGuiana": FrenchGuianaFlag,
+    "Gabon": GabonFlag,
+    "Gambia": GambiaFlag,
+    "Georgia": GeorgiaFlag,
+    "Germany": GermanyFlag,
+    "Ghana": GhanaFlag,
+    "Gibraltar": GibraltarFlag,
+    "Greece": GreeceFlag,
+    "Grenada": GrenadaFlag,
+    "Guadeloupe": GuadeloupeFlag,
+    "Guatemala": GuatemalaFlag,
+    "Guemsey": GuemseyFlag,
+    "Guinea": GuineaFlag,
+    "Guyana": GuyanaFlag,
+    "Haiti": HaitiFlag,
+    "Honduras": HondurasFlag,
+    "Hong Kong": HongKongFlag,
+    "Hungary": HungaryFlag,
+    "Iceland": IcelandFlag,
+    "India": IndiaFlag,
+    "Indonesia": IndonesiaFlag,
+    "Iran": IranFlag,
+    "Iraq": IraqFlag,
+    "Ireland": IrelandFlag,
+    "IsleOfMan": IsleOfManFlag,
+    "Israel": IsraelFlag,
+    "Italy": ItalyFlag,
+    "IvoryCoast": IvoryCoastFlag,
+    "Jamaica": JamaicaFlag,
+    "Japan": JapanFlag,
+    "Jersey": JerseyFlag,
+    "Jordan": JordanFlag,
+    "Kazakhstan": KazakhstanFlag,
+    "Kenya": KenyaFlag,
+    "Kiribati": KiribatiFlag,
+    "Kosovo": KosovoFlag,
+    "Kuwait": KuwaitFlag,
+    "Kyrgyzstan": KyrgyzstanFlag,
+    "Laos": LaosFlag,
+    "Latvia": LatviaFlag,
+    "Lebanon": LebanonFlag,
+    "Lesotho": LesothoFlag,
+    "Liberia": LiberiaFlag,
+    "Libya": LibyaFlag,
+    "Liechtenstein": LiechtensteinFlag,
+    "Lithuania": LithuaniaFlag,
+    "Luxembourg": LuxembourgFlag,
+    "Madagascar": MadagascarFlag,
+    "Malawi": MalawiFlag,
+    "Malaysia": MalaysiaFlag,
+    "Maldives": MaldivesFlag,
+    "Mali": MaliFlag,
+    "Malta": MaltaFlag,
+    "MarshallIslands": MarshallIslandsFlag,
+    "Mauritiana": MauritianaFlag,
+    "Mauritius": MauritiusFlag,
+    "Mexico": MexicoFlag,
+    "Micronesia": MicronesiaFlag,
+    "Moldova": MoldovaFlag,
+    "Monaco": MonacoFlag,
+    "Mongolia": MongoliaFlag,
+    "Montenegro": MontenegroFlag,
+    "Morocco": MoroccoFlag,
+    "Mozanbique": MozanbiqueFlag,
+    "Myanmar": MyanmarFlag,
+    "Namibia": NamibiaFlag,
+    "Nauru": NauruFlag,
+    "Nepal": NepalFlag,
+    "The Netherlands": NetherlandsFlag,
+    "New Zealand": NewZealandFlag,
+    "Nicaragua": NicaraguaFlag,
+    "Niger": NigerFlag,
+    "Nigeria": NigeriaFlag,
+    "NorthKorea": NorthKoreaFlag,
+    "NorthMacedonia": NorthMacedoniaFlag,
+    "Norway": NorwayFlag,
+    "Oman": OmanFlag,
+    "Pakistan": PakistanFlag,
+    "Palau": PalauFlag,
+    "Palestine": PalestineFlag,
+    "Panama": PanamaFlag,
+    "PapuaNewGuinea": PapuaNewGuineaFlag,
+    "Paraguay": ParaguayFlag,
+    "Peru": PeruFlag,
+    "Philippines": PhilippinesFlag,
+    "Poland": PolandFlag,
+    "Portugal": PortugalFlag,
+    "PuertoRico": PuertoRicoFlag,
+    "Qatar": QatarFlag,
+    "RepublicCongo": RepublicCongoFlag,
+    "Romania": RomaniaFlag,
+    "Russia": RussiaFlag,
+    "Rwanda": RwandaFlag,
+    "SaintKittsNevis": SaintKittsNevisFlag,
+    "SaintLucia": SaintLuciaFlag,
+    "SaintVincentGrenadines": SaintVincentGrenadinesFlag,
+    "Samoa": SamoaFlag,
+    "SanMarino": SanMarinoFlag,
+    "SaoTomePrincipe": SaoTomePrincipeFlag,
+    "Saudi Arabia": SaudiArabiaFlag,
+    "Senegal": SenegalFlag,
+    "Serbia": SerbiaFlag,
+    "Seychelles": SeychellesFlag,
+    "SierraLeone": SierraLeoneFlag,
+    "Singapore": SingaporeFlag,
+    "Slovakia": SlovakiaFlag,
+    "Slovenia": SloveniaFlag,
+    "SolomonIslands": SolomonIslandsFlag,
+    "Somalia": SomaliaFlag,
+    "SouthAfrica": SouthAfricaFlag,
+    "SouthGeorgiaSandwichIslands": SouthGeorgiaSandwichIslandsFlag,
+    "SouthKorea": SouthKoreaFlag,
+    "SouthSudan": SouthSudanFlag,
+    "Spain": SpainFlag,
+    "SriLanka": SriLankaFlag,
+    "Sudan": SudanFlag,
+    "Suriname": SurinameFlag,
+    "Sweden": SwedenFlag,
+    "Switzerland": SwitzerlandFlag,
+    "Syria": SyriaFlag,
+    "Taiwan": TaiwanFlag,
+    "Tajikistan": TajikistanFlag,
+    "Tanzania": TanzaniaFlag,
+    "Thailand": ThailandFlag,
+    "Togo": TogoFlag,
+    "Tonga": TongaFlag,
+    "TrinidadTobago": TrinidadTobagoFlag,
+    "Tunisia": TunisiaFlag,
+    "Turkey": TurkeyFlag,
+    "Turkmenistan": TurkmenistanFlag,
+    "Uganda": UgandaFlag,
+    "Ukraine": UkraineFlag,
+    "United Arab Emirates": UnitedArabEmiratesFlag,
+    "United Kingdom": UnitedKingdomFlag,
+    "United States": UnitedStatesFlag,
+    "Uruguay": UruguayFlag,
+    "Uzbekistan": UzbekistanFlag,
+    "Venezuela": VenezuelaFlag,
+    "Vietnam": VietnamFlag,
+    "Yemen": YemenFlag,
+    "Zambia": ZambiaFlag,
+}
